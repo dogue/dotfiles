284efd080736d522248827b39f0c9f2d478134b2
Dogue <dogue@oddhelix.com>
2023-01-11 18:28:01 -0500

    add nvim bind `<leader>cf` for `cargo fmt`

--- dot_config/nvim/lua/dogue/remap.lua
+++ dot_config/nvim/lua/dogue/remap.lua
@@ -17,3 +17,5 @@ vim.keymap.set("n", "<leader>Y", [["+y]])
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>cf", ":! cargo fmt<CR><CR>")
